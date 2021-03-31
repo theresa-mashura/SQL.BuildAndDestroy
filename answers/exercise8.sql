8775f3cd5c46ea4bca86fad125edd820cc567b5d
theresa-mashura <78986299+theresa-mashura@users.noreply.github.com>
2021-03-31 00:51:34 -0400

    Update exercise8.sql

---
+++ answers/exercise8.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Students (
+    StudentName VARCHAR(50), 
+    Address VARCHAR(50), 
+    City VARCHAR(50), 
+    PostalCode VARCHAR(50), 
+    Country VARCHAR(50) );
+
+INSERT INTO Students 
+  VALUES 
+    ("Jane Doe", "57 Union Street", "Glaswgow", "G13RB", "Scotland"); 
